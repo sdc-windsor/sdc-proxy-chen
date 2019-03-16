1fc42305ef612dfe81140ba98fb89993b0f8f4fb
Chen Wilson <chenlwilson@gmail.com>
2019-03-16 15:38:16 -0700

    Corrected urls

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.static('public'));
 app.use('/:id', express.static('public'));
+app.use(cors());
 
 module.exports = app;
